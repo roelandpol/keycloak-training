3f8bd85fb6c905066355696c13923b2ad6f146d6
roeland <roelandpol@gmail.com>
2024-10-08 13:02:15 +0200

    upd

--- upgrade-lab.sh
+++ upgrade-lab.sh
@@ -12,8 +12,11 @@ git -C $SCRIPT_DIR lfs install
 git -C $SCRIPT_DIR lfs fetch
 git -C $SCRIPT_DIR lfs checkout
 
-# Copy RHBK to home direcotry
-cp $SCRIPT_DIR/rhbk-24.0.8.zip $HOME
+# Replace SSO version on student machine
+ssh student@sso -- rm *.zip
+scp $SCRIPT_DIR/rhbk-24.0.8.zip student@sso:
 
+# Replace playbooks
+cp $SCRIPT_DIR/install-sso-server.yaml .venv/labs/lib/python3.6/site-packages/do313/ansible/common/
 
 
